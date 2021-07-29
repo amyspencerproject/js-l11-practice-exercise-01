d1d3a17c4eea67668b91f1f8e9e7c0085855c7d1
amyspencerproject <amy@spencerproject.com>
2021-07-29 17:52:15 -0600

    Added code to pull user data from API and put into an array

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+    const usersRequest = await fetch("https://randomuser.me/api/?results=5");
+    const data = await usersRequest.json();
+    // console.log(data);
+    const usersResults = data.results; 
+    //data contains all the json files info, data.results parses down info to just array 
+    console.log(usersResults);
+};
+
+getData();
+
+const displayUsers = function (usersResults) {
+    randomFolks.innerHTML("");
+};
