696fcb3d364b14be67a46369fdea13bbdc863742
amyspencerproject <amy@spencerproject.com>
2021-07-29 18:30:35 -0600

    Used for of loop to select and style data for website view

--- js/index.js
+++ js/index.js
@@ -6,11 +6,30 @@ const getData = async function () {
     // console.log(data);
     const usersResults = data.results; 
     //data contains all the json files info, data.results parses down info to just array 
-    console.log(usersResults);
+    // console.log(usersResults);
+
+    displayUsers(usersResults);
 };
 
 getData();
 
 const displayUsers = function (usersResults) {
-    randomFolks.innerHTML("");
+    randomFolks.innerHTML="";
+
+    for (const user of usersResults) {
+        const country = user.location.country;
+        const firstName = user.name.first;
+        const imageUrl = user.picture.medium;
+        const userDiv = document.createElement("div");
+        userDiv.innerHTML = `
+            <h3>${firstName}</h3>
+            <p>${country}</p>
+            <img src=${imageUrl} alt="User's Avatar" />
+        `;
+    randomFolks.append(userDiv)
+    }
+
 };
+
+
+
